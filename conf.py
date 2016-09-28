67e6505e814667a643dc2dd790f7a5e209ef13de
Lena (zansorgova) <zuzana.ansorgova@nic.cz>
2016-09-28 14:58:59 +0200

    Configured TODO extension

--- conf.py
+++ conf.py
@@ -36,6 +36,11 @@ extensions = [
     'sphinx.ext.ifconfig',
 ]
 
+# Extensions configuration - TODOs
+todo_include_todos = True
+#todo_link_only = True
+
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
